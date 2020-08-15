c046512469496291c04209f307121aff4b8d9ab8
Mochamad Taufik Romdony <mtaufik.romdony@gmail.com>
2020-08-16 00:24:37 +0700

    Add alicloud provider tf

---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "alicloud" {
+  # Here you can find the "Region ID": https://www.alibabacloud.com/help/doc-detail/40654.htm
+  region     = var.region
+
+  # How to create a pair of access_key and secret_key: https://www.alibabacloud.com/help/doc-detail/53045.htm
+  access_key = ""
+  secret_key = ""
+}
